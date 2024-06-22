f870dcc17ffbb8e632468619bc7da64a3e9e370a
xvoorvaa <xander@xvproductions.com>
2024-06-22 13:50:52 +0200

    Changed malloc to ft_malloc

--- tests/src/test.c
+++ tests/src/test.c
@@ -1,39 +1,34 @@
-/*#ifdef __APPLE__*/
-/*#include <malloc/malloc.h>*/
-/*#endif*/
-/**/
-/*#ifdef linux*/
-/*#include <malloc.h>*/
-/*#endif*/
+#ifdef __APPLE__
+#include <malloc/malloc.h>
+#endif
+
+#ifdef linux
+#include <malloc.h>
+#endif
 
 #include "libft_plus.h"
 
+#include <assert.h>
 #include <string.h>
 
 void fill_string(char *ptr, int64_t n) {
   for (int i = 0; i < n; i++) {
     ptr[i] = (i % 26) + 'A';
   }
+
+  ptr[n] = '\0';
 }
 
 int32_t main(void) {
-  char *test = ft_malloc(0);
-
-  printf("test = %s\n", test);
+  assert(ft_malloc(0) == NULL);
 
   char *ptr = ft_malloc(sizeof(char) * 320);
-  char *another_ptr = ft_malloc(320);
 
   // Should overflow
-  fill_string(ptr, 319);
+  fill_string(ptr, 10);
 
-  ptr[319] = '\0';
-  another_ptr[319] = '\0';
   printf("Malloc: %s\n", ptr);
   printf("strlen: %ld\n", strlen(ptr));
-
-  printf("Malloc: %s\n", another_ptr);
-  printf("strlen: %ld\n", strlen(another_ptr));
 
 #ifdef linux
   /*size = malloc_usable_size(ptr);*/
