496716b593ba3f6f2a57216b6a9685443a308b90
Xannyx <xander@xvproductions.com>
2024-07-19 15:57:52 +0200

    Added a MACRO, and refactored

--- src/malloc.c
+++ src/malloc.c
@@ -1,85 +1,57 @@
-#include <pthread.h>
-#include <sched.h>
-#include <stdint.h>
-#include <stdio.h>
-#include <unistd.h>
-
 #include "alloc.h"
 #include "debug.h"
 #include "libft_plus.h"
 #include "shared.h"
+#include <pthread.h>
+#include <stdint.h>
+#include <stdio.h>
+
+static t_block *allocate_memory(t_heap **heap, size_t aligned_size) {
+  t_heap *tmp_heap = *heap;
+  size_t block_size = determine_total_block_size(aligned_size);
+
+  while (tmp_heap->next) {
+    tmp_heap = tmp_heap->next;
+  }
+
+  if ((int64_t)block_size > tmp_heap->free_size) {
+    DEBUG_PRINT_SIMPLE("Call mmap()");
+    return extend_heap(heap, aligned_size);
+  }
+
+  if (any_block_not_inuse(*heap)) {
+    DEBUG_PRINT_SIMPLE("Reuse blocks");
+    return reuse_block(&tmp_heap, aligned_size);
+  }
+
+  DEBUG_PRINT_SIMPLE("Add blocks");
+  return extend_blocks(&tmp_heap, aligned_size);
+}
 
 void *ft_malloc(size_t size) {
-  if (size <= 0) {
+  if (size == 0) {
     return NULL;
   }
 
-  t_heap *heap = g_heap;
-
   pthread_mutex_lock(&g_mutex);
 
-  t_block *block = NULL;
   const size_t aligned_size = align(size);
   const size_t amount_of_blocks = determine_block_size(aligned_size);
   const size_t determined_heap_size =
       determine_heap_size(aligned_size, amount_of_blocks);
 
-  // Add new blocks if heap is empty or heap_size will be bigger than
-  // SMALL_HEAP_ALLOCATION_SIZE
-  if (!heap || (determined_heap_size > (size_t)SMALL_HEAP_ALLOCATION_SIZE)) {
-
-#ifdef DEBUG
-    printf("extending heap\n");
-#endif
-
-    block = extend_heap(&heap, aligned_size);
+  t_block *block;
+  if (!g_heap || (determined_heap_size > (size_t)SMALL_HEAP_ALLOCATION_SIZE)) {
+    DEBUG_PRINT_SIMPLE("Extending heap");
+    block = extend_heap(&g_heap, aligned_size);
   } else {
-    t_heap *tmp_heap = heap;
-    size_t block_size = determine_total_block_size(aligned_size);
-
-    while (tmp_heap->next) {
-      tmp_heap = tmp_heap->next;
-    }
-
-    // Check if there is enough free space in the last heap, if not create a new
-    // one
-    if ((int64_t)block_size > tmp_heap->free_size) {
-
-#ifdef DEBUG
-      printf("Call mmap()\n");
-#endif
-
-      block = extend_heap(&heap, aligned_size);
-    } else {
-      bool inuse = any_block_not_inuse(heap);
-
-      // Check if we can reuse a block
-      if (inuse == true) {
-#ifdef DEBUG
-        printf("Reuse blocks\n");
-#endif
-        block = reuse_block(&tmp_heap, aligned_size);
-      } else {
-#ifdef DEBUG
-        printf("Add blocks\n");
-#endif
-        // Otherwise create new blocks
-        block = extend_blocks(&tmp_heap, aligned_size);
-      }
-      heap = tmp_heap;
-    }
+    block = allocate_memory(&g_heap, aligned_size);
   }
 
   if (!block) {
     pthread_mutex_unlock(&g_mutex);
     return NULL;
   }
-
-  g_heap = heap;
-
-  /*size_t *end_canary =*/
-  /*    (size_t *)((char *)block + block->size + sizeof(t_block));*/
-  /**end_canary = BLOCK_MAGIC;*/
 
   pthread_mutex_unlock(&g_mutex);
   return (void *)((char *)block + sizeof(t_block));
