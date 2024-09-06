7a164c0d061d6bbaef3ac2f001d3a75dc83585f8
xvoorvaa <xander@xvproductions.com>
2024-09-06 14:33:22 +0200

    Made realloc functional

--- src/realloc.c
+++ src/realloc.c
@@ -4,13 +4,19 @@
 
 #include <pthread.h>
 #include <stdlib.h>
+#include <string.h>
 
 void *ft_realloc(void *ptr, size_t size) {
   pthread_mutex_lock(&g_mutex);
 
   if (!ptr) {
-    write(STDERR_FILENO, "ptr empty\n", 11);
+    void *new_ptr = ft_malloc(size);
+    pthread_mutex_unlock(&g_mutex);
+    return new_ptr;
+  }
 
+  if (size == 0) {
+    ft_free(ptr);
     pthread_mutex_unlock(&g_mutex);
     return NULL;
   }
@@ -26,8 +32,16 @@ void *ft_realloc(void *ptr, size_t size) {
   t_block *block = (t_block *)((char *)ptr - sizeof(t_block));
   t_heap *heap = find_heap_for_block(block);
 
-  if ((size_t)heap->free_size >= aligned_size) {
+  if (block->size >= aligned_size) {
+    pthread_mutex_unlock(&g_mutex);
+    return ptr;
+  }
+
+  if ((size_t)heap->free_size >= (aligned_size - block->size)) {
+    size_t old_size = block->size;
+
     block->size = aligned_size;
+    heap->free_size -= (aligned_size - old_size);
 
     uint32_t *magic_end =
         (uint32_t *)((char *)block + SIZEOF_BLOCK + block->size);
@@ -37,9 +51,16 @@ void *ft_realloc(void *ptr, size_t size) {
     return ptr;
   }
 
-  t_block *heap_block = extend_heap(&heap, true, aligned_size);
-  (void)heap_block;
+  void *new_ptr = ft_malloc(size);
+  if (!new_ptr) {
+    pthread_mutex_unlock(&g_mutex);
+    return NULL;
+  }
+
+  memcpy(new_ptr, ptr, block->size);
+
+  ft_free(ptr);
 
   pthread_mutex_unlock(&g_mutex);
-  return ptr;
+  return new_ptr;
 }
